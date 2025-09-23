c00752227bd6917ad067729901ff0649cf1ac8d1
Dan Rumney <dan@sutro.xyz>
2025-09-23 16:31:15 -0500

    update declaration file

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,5 +2,8 @@ export declare class Route {
   constructor(route: string);
   match(path: string): Record<string, string> | false;
   reverse(params: Record<string, string>): string;
+  generate(
+    generators: Array<{ match: RegExp; generate: () => string }>
+  ): string;
   readonly spec: string;
 }
